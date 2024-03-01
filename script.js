3f3e5955b39770f612e4624802d0881122fbfaa2
Nayeem-condify <webdevlopment2306@gmail.com>
2024-03-01 23:41:25 +0600

    2nd commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let number1 = document.querySelector(".number1")
+let number2 = document.querySelector(".number2")
+
+let plus = document.querySelector(".plus")
+let minuse = document.querySelector(".minuse")
+let gun = document.querySelector(".gun")
+let vag = document.querySelector(".vag")
+
+let result = document.querySelector(".result")
+
+
+plus.addEventListener("click", function(){
+    console.log(number1);
+    console.log(number2);
+})
